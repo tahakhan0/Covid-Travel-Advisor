c8a440106122409d0edb09686c6944787716fa4d
Taha Khan <mac@macs-MacBook-Pro.local>
2020-11-02 03:31:49 +0300

    added some images

--- src/components/images/image.js
+++ src/components/images/image.js
@@ -2,6 +2,10 @@ import React from "react";
 import usa from "./US.png";
 import india from "./IN.png";
 import brazil from "./BR.png";
+import france from "./FR.png";
+import russia from "./RU.png";
+import argentina from "./AR.png";
+import spain from "./ES.png";
 
 const styleForImages = {
   height: "40px",
@@ -14,11 +18,25 @@ const US = (
 );
 
 const BR = (
-  <img src={brazil} style={styleForImages} alt="US-travel-corona-assitant" />
+  <img src={brazil} style={styleForImages} alt="BR-travel-corona-assitant" />
 );
 
 const IN = (
-  <img src={india} style={styleForImages} alt="US-travel-corona-assitant" />
+  <img src={india} style={styleForImages} alt="IN-travel-corona-assitant" />
 );
 
-export { US, BR, IN };
+const FR = (
+  <img src={france} style={styleForImages} alt="FR-travel-corona-assitant" />
+);
+
+const RU = (
+  <img src={russia} style={styleForImages} alt="RU-travel-corona-assitant" />
+);
+const ES = (
+  <img src={spain} style={styleForImages} alt="ES-travel-corona-assitant" />
+);
+const AR = (
+  <img src={argentina} style={styleForImages} alt="AR-travel-corona-assitant" />
+);
+
+export { US, BR, IN, FR, RU, ES, AR };
